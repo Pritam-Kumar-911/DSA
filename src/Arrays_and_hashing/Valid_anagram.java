e4bf03de7b7e6c286e8b4191a4a2dcd10987cb62
Pritam Kumar <pritam03128910155@gmail.com>
2026-07-22 14:59:24 +0500

    Solved Valid Anagram Problem

--- src/Arrays_and_hashing/Valid_anagram.java
+++ src/Arrays_and_hashing/Valid_anagram.java
@@ -1,23 +1,37 @@
 package Arrays_and_hashing;
 
+import java.util.HashMap;
 import java.util.HashSet;
+import java.util.Map;
 
 public class Valid_anagram {
     public static void main(String[] args) {
-        String s = "rat";
-        String t = "car";
-        HashSet<Character> set = new HashSet<>();
+        String s = "anagram";
+        String t = "nagaram";
+
+        int count = s.length();
+
+        HashMap<Character , Integer> map = new HashMap<>();
         boolean flag = true;
 
-        for (int i=0 ; i<s.length() ; i++){
-            char c = s.charAt(i);
-            set.add(c);
+        if (s.length() != t.length()){
+            System.out.println("False");
         }
-        for (int i=0; i<t.length() ; i++){
-            if (!set.contains(t.charAt(i))){
-                flag = false;
+
+        for (int i=0 ; i<count ; i++){
+            map.put(s.charAt(i) , map.getOrDefault(s.charAt(i) , 0) + 1);
+        }
+        for (int i=0 ; i<count ; i++){
+            map.put(t.charAt(i) , map.getOrDefault(t.charAt(i) , 0) - 1);
+        }
+
+        for (int value: map.values()){
+            if (value != 0){
+              flag = false;
             }
         }
+
         System.out.println(flag);
+
     }
 }
