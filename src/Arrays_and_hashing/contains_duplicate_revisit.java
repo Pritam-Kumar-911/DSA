c860267be5b764934efac86cf562a6b72f1869bf
Pritam Kumar <pritam03128910155@gmail.com>
2026-07-31 22:25:38 +0500

    Revisited Contains duplicate Problem Confidence (5/5)

---
+++ src/Arrays_and_hashing/contains_duplicate_revisit.java
@@ -0,0 +1,21 @@
+package Arrays_and_hashing;
+
+import java.util.HashSet;
+
+public class contains_duplicate_revisit {
+    public static void main(String[] args) {
+        int[] nums = {1,1,1,3,3,4,3,2,4,2};
+        HashSet<Integer> set = new HashSet<>();
+        boolean flag = false;
+
+        for (int arr: nums){
+            if (set.contains(arr)){
+                flag = true;
+            }else {
+                set.add(arr);
+            }
+        }
+
+        System.out.println(flag);
+    }
+}
