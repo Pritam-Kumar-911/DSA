c280916338ca1d6302d727b6eb8756827682e085
Pritam Kumar <pritam03128910155@gmail.com>
2026-08-04 00:41:30 +0500

    Solved Container with most water problem optimaly via Two Pointers

--- src/TwoPointers/Container_with_most_water.java
+++ src/TwoPointers/Container_with_most_water.java
@@ -5,18 +5,38 @@ public class Container_with_most_water {
         int[] height = {1,8,6,2,5,4,8,3,7};
         int output = 0;
 
-        for(int i=0 ; i<height.length-1 ; i++){
+        // for(int i=0 ; i<height.length-1 ; i++){
 
-            for(int j=i+1 ; j<height.length ; j++){
-                int length = Math.min(height[i], height[j]);
-                int width = j - i;
+        //     for(int j=i+1 ; j<height.length ; j++){
+        //         int length = Math.min(height[i], height[j]);
+        //         int width = j - i;
                 
-                int total = length*width;
+        //         int total = length*width;
 
-                output = Math.max(output, total);
+        //         output = Math.max(output, total);
 
+        //     }
+        // }
+
+        int left = 0;
+        int right = height.length-1;
+
+        while (left < right) {
+
+            int length = Math.min(height[left], height[right]);
+            int width = right - left;
+                
+            int total = length*width;
+
+            output = Math.max(output, total);
+
+            if (height[left] < height[right]) {
+                left++;
+            }else{
+                right--;
             }
         }
+
         System.out.println(output);
     }
 }
